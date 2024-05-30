77c2419b64006f8b2cbf4a4147c54eaf65b6aa72
storms <storms@uia.no>
2024-05-30 10:09:39 +0000

    Finished with the musicplayer

--- src/48_GuttaKrutt/src/kernel.cpp
+++ src/48_GuttaKrutt/src/kernel.cpp
@@ -6,6 +6,12 @@ extern "C"{
     #include "input.h"
 }
 
+SongPlayer* create_song_player() {
+    auto* player = new SongPlayer();
+    player->play_song = play_song_impl;
+    return player;
+}
+
 extern "C" int kernel_main(void);
 int kernel_main(){
 
@@ -60,6 +66,27 @@ int kernel_main(){
         // Acknowledge the IRQ
         asm volatile("cli");
     }, NULL);
+
+ Song* songs[] = {
+
+        new Song({battlefield_1942_theme, sizeof(battlefield_1942_theme) / sizeof(Note)}),
+        new Song({starwars_theme, sizeof(starwars_theme) / sizeof(Note)}),
+        new Song({music_1, sizeof(music_1) / sizeof(Note)}),    
+        new Song({music_6, sizeof(music_6) / sizeof(Note)}),
+        new Song({music_5, sizeof(music_5) / sizeof(Note)}),
+        new Song({music_4, sizeof(music_4) / sizeof(Note)}),
+        new Song({music_3, sizeof(music_3) / sizeof(Note)}),
+        new Song({music_2, sizeof(music_2) / sizeof(Note)})
+    };
+    uint32_t n_songs = sizeof(songs) / sizeof(Song*);
+
+    // Create a song player and play each song
+    SongPlayer* player = create_song_player();
+    for(uint32_t i = 0; i < n_songs; i++) {
+        printf("Playing Song...\n");
+        player->play_song(songs[i]);
+        printf("Finished playing the song.\n");
+    }
 
     // Main loop
     printf("Kernel main loop\n");
